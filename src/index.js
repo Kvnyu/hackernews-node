5f1501a98cb8bb2f152adc0104de441a589aacbc
Kvnyu <52982849+Kvnyu@users.noreply.github.com>
2020-12-24 15:31:28 +1100

    added prisma

--- src/index.js
+++ src/index.js
@@ -1,46 +1,84 @@
 const {ApolloServer} = require('apollo-server');
-
-const typeDefs = `
-  type Query {
-    info: String!
-    feed: [Link!]!
-    }
-  
-  type Mutation {
-    post(url: String!, description: String!) : Link! 
-  }
-   
-  type Link {
-     id: ID!
-     description: String!
-     url: String! 
-     } 
-  `
+const fs = require('fs');
+const path = require('path');
+const {PrismaClient} = require('@prisma/client')
 
 let links = [{
     id: 'link-0',
     url: 'www.howtographql.com',
     description: 'Fullstack tutorial for GraphQL'
 }]
-
+let idCount = links.length
 
 const resolvers = {
     Query: {
         info: () => "This is a Hackernews clone",
-        feed: () => links,
+        feed: () => async (parent, args, context) => {
+            return context.prisma.link.findMany()
+        },
+        link: (parent, args) => {
+            let link = links.filter(
+                (link) => (link.id == args.id)
+            )
+            console.log(link)
+            return link[0]
+            // console.log(id)
+            // console.log( links.filter((link) => link.id = id))
+            // return links.filter((link) => link.id = id)[0]
+
+        }
     },
+    Mutation: {
+        post: (parent, args) => {
+            const link = {
+                id: `link-${idCount++}`,
+                description: args.description,
+                url: args.url
 
-    Link: {
-        id: (parent) => parent.id,
-        description: (parent) => parent.description,
-        url: (parent) => parent.url,
-
+            }
+            links.push(link)
+            return link
+        },
+        updateLink: (parent, args) => {
+            var updated = false
+            updatedLink = {
+                id: args.id,
+                url: args.url,
+                description: args.description
+            }
+            links.forEach((link) => {
+                console.log(link)
+                if (link.id == args.id) {
+                    link.id = args.id,
+                        link.url = args.url,
+                    link.description = args.description
+                    updated = true
+                    return link
+                }
+                else{
+                    return link
+                }
+            })
+            return updated ? updatedLink : "Failed to update"
+        },
+        deleteLink: (parent, args) => {
+           let links = links.filter((link) => !(link.id = id) )
+        }
     }
+
 }
 
+const prisma = new PrismaClient()
+
 const server = new ApolloServer({
-    typeDefs,
+    typeDefs: fs.readFileSync(
+        path.join(__dirname, 'schema.graphql'),
+        'utf8'
+    ),
     resolvers,
+    context: {
+        prisma,
+    }
 })
 
 server
