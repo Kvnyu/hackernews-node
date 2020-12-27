e5bfcb162b4670284dfda7bebfa005d802e70109
Kvnyu <52982849+Kvnyu@users.noreply.github.com>
2020-12-27 11:18:44 +1100

    Tutorial finished!

--- src/resolvers/query.js
+++ src/resolvers/query.js
@@ -21,8 +21,17 @@ async function feed (parent, args, context, info){
     //The above creates the filter for the prisma query
     const links = await context.prisma.link.findMany({
         where,
+        skip: args.skip,
+        take: args.take,
+        orderBy: args.orderBy
     })
-    return links
+
+    const count = await context.prisma.link.count({where})
+
+    return {
+        links,
+        count,
+    }
 }
 
 module.exports = {
